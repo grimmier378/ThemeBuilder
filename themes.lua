f9334c6f8db418474e5f2f3a73e67e461646c43c
grimmier378 <124466615+grimmier378@users.noreply.github.com>
2024-03-17 22:55:25 -0600

    Cleanup:
    Cleaned up some code.
    
    now instead of hard coding every color we build the statement from the table as we iterate through it.
    
    Added the text value for the property name to the table. for display

--- themes.lua
+++ themes.lua
@@ -4,61 +4,226 @@ return {
         [1] = {
             ['Name'] = 'Default',
             ['Color'] = {
-                [ImGuiCol.Text]                   = {1.00, 1.00, 1.00, 1.00},
-                [ImGuiCol.TextDisabled]           = {0.50, 0.50, 0.50, 1.00},
-                [ImGuiCol.WindowBg]               = {0.06, 0.06, 0.06, 0.94},
-                [ImGuiCol.ChildBg]                = {0.00, 0.00, 0.00, 0.00},
-                [ImGuiCol.PopupBg]                = {0.08, 0.08, 0.08, 0.94},
-                [ImGuiCol.Border]                 = {0.43, 0.43, 0.50, 0.50},
-                [ImGuiCol.BorderShadow]           = {0.00, 0.00, 0.00, 0.00},
-                [ImGuiCol.FrameBg]                = {0.16, 0.29, 0.48, 0.54},
-                [ImGuiCol.FrameBgHovered]         = {0.26, 0.59, 0.98, 0.40},
-                [ImGuiCol.FrameBgActive]          = {0.26, 0.59, 0.98, 0.67},
-                [ImGuiCol.TitleBg]                = {0.04, 0.04, 0.04, 1.00},
-                [ImGuiCol.TitleBgActive]          = {0.16, 0.29, 0.48, 1.00},
-                [ImGuiCol.TitleBgCollapsed]       = {0.00, 0.00, 0.00, 0.51},
-                [ImGuiCol.MenuBarBg]              = {0.14, 0.14, 0.14, 1.00},
-                [ImGuiCol.ScrollbarBg]            = {0.02, 0.02, 0.02, 0.53},
-                [ImGuiCol.ScrollbarGrab]          = {0.31, 0.31, 0.31, 1.00},
-                [ImGuiCol.ScrollbarGrabHovered]   = {0.41, 0.41, 0.41, 1.00},
-                [ImGuiCol.ScrollbarGrabActive]    = {0.51, 0.51, 0.51, 1.00},
-                [ImGuiCol.CheckMark]              = {0.26, 0.59, 0.98, 1.00},
-                [ImGuiCol.SliderGrab]             = {0.24, 0.52, 0.88, 1.00},
-                [ImGuiCol.SliderGrabActive]       = {0.26, 0.59, 0.98, 1.00},
-                [ImGuiCol.Button]                 = {0.26, 0.59, 0.98, 0.40},
-                [ImGuiCol.ButtonHovered]          = {0.26, 0.59, 0.98, 1.00},
-                [ImGuiCol.ButtonActive]           = {0.06, 0.53, 0.98, 1.00},
-                [ImGuiCol.Header]                 = {0.26, 0.59, 0.98, 0.31},
-                [ImGuiCol.HeaderHovered]          = {0.26, 0.59, 0.98, 0.80},
-                [ImGuiCol.HeaderActive]           = {0.26, 0.59, 0.98, 1.00},
-                [ImGuiCol.Separator]              = {0.43, 0.43, 0.50, 0.50},
-                [ImGuiCol.SeparatorHovered]       = {0.10, 0.40, 0.75, 0.78},
-                [ImGuiCol.SeparatorActive]        = {0.10, 0.40, 0.75, 1.00},
-                [ImGuiCol.ResizeGrip]             = {0.26, 0.59, 0.98, 0.20},
-                [ImGuiCol.ResizeGripHovered]      = {0.26, 0.59, 0.98, 0.67},
-                [ImGuiCol.ResizeGripActive]       = {0.26, 0.59, 0.98, 0.95},
-                [ImGuiCol.Tab]                    = {0.18, 0.35, 0.58, 0.86},
-                [ImGuiCol.TabHovered]             = {0.26, 0.59, 0.98, 0.80},
-                [ImGuiCol.TabActive]              = {0.20, 0.41, 0.68, 1.00},
-                [ImGuiCol.TabUnfocused]           = {0.07, 0.10, 0.15, 0.97},
-                [ImGuiCol.TabUnfocusedActive]     = {0.14, 0.26, 0.42, 1.00},
-                [ImGuiCol.DockingPreview]         = {0.26, 0.59, 0.98, 0.70},
-                [ImGuiCol.DockingEmptyBg]         = {0.20, 0.20, 0.20, 1.00},
-                [ImGuiCol.PlotLines]              = {0.61, 0.61, 0.61, 1.00},
-                [ImGuiCol.PlotLinesHovered]       = {1.00, 0.43, 0.35, 1.00},
-                [ImGuiCol.PlotHistogram]          = {0.90, 0.70, 0.00, 1.00},
-                [ImGuiCol.PlotHistogramHovered]   = {1.00, 0.60, 0.00, 1.00},
-                [ImGuiCol.TableHeaderBg]          = {0.19, 0.19, 0.20, 1.00},
-                [ImGuiCol.TableBorderStrong]      = {0.31, 0.31, 0.35, 1.00},
-                [ImGuiCol.TableBorderLight]       = {0.23, 0.23, 0.25, 1.00},
-                [ImGuiCol.TableRowBg]             = {0.00, 0.00, 0.00, 0.00},
-                [ImGuiCol.TableRowBgAlt]          = {1.00, 1.00, 1.00, 0.06},
-                [ImGuiCol.TextSelectedBg]         = {0.26, 0.59, 0.98, 0.35},
-                [ImGuiCol.DragDropTarget]         = {1.00, 1.00, 0.00, 0.90},
-                [ImGuiCol.NavHighlight]           = {0.26, 0.59, 0.98, 1.00},
-                [ImGuiCol.NavWindowingHighlight]  = {1.00, 1.00, 1.00, 0.70},
-                [ImGuiCol.NavWindowingDimBg]      = {0.80, 0.80, 0.80, 0.20},
-                [ImGuiCol.ModalWindowDimBg]       = {0.80, 0.80, 0.80, 0.35}
+                [ImGuiCol.Text]                   = {
+                    Color = {1.00, 1.00, 1.00, 1.00},
+                    PropertyName = "Text"
+                },
+                [ImGuiCol.TextDisabled]           = {
+                    Color = {0.50, 0.50, 0.50, 1.00},
+                    PropertyName = 'TextDisabled'
+                },
+                [ImGuiCol.WindowBg]               = {
+                    Color = {0.06, 0.06, 0.06, 0.94},
+                    PropertyName = 'WindowBg'
+                },
+                [ImGuiCol.ChildBg]                = {
+                    Color = {0.00, 0.00, 0.00, 0.00},
+                    PropertyName = 'ChildBg'
+                },
+                [ImGuiCol.PopupBg]                = {
+                    Color = {0.08, 0.08, 0.08, 0.94},
+                    PropertyName = 'PopupBg'
+                },
+                [ImGuiCol.Border]                 = {
+                    Color = {0.43, 0.43, 0.50, 0.50},
+                    PropertyName = 'Border'
+                },
+                [ImGuiCol.BorderShadow]           = {
+                    Color = {0.00, 0.00, 0.00, 0.00},
+                    PropertyName = 'BorderShadow'
+                },
+                [ImGuiCol.FrameBg]                = {
+                    Color = {0.16, 0.29, 0.48, 0.54},
+                    PropertyName = 'FrameBg'
+                },
+                [ImGuiCol.FrameBgHovered]         = {
+                    Color = {0.26, 0.59, 0.98, 0.40},
+                    PropertyName = 'FrameBgHovered'
+                },
+                [ImGuiCol.FrameBgActive]          = {
+                    Color = {0.26, 0.59, 0.98, 0.67},
+                    PropertyName = 'FrameBgActive'
+                },
+                [ImGuiCol.TitleBg]                = {
+                    Color = {0.04, 0.04, 0.04, 1.00},
+                    PropertyName = 'TitleBg'
+                },
+                [ImGuiCol.TitleBgActive]          = {
+                    Color = {0.16, 0.29, 0.48, 1.00},
+                    PropertyName = 'TitleBgActive'
+                },
+                [ImGuiCol.TitleBgCollapsed]       = {
+                    Color = {0.00, 0.00, 0.00, 0.51},
+                    PropertyName = 'TitleBgCollapsed'
+                },
+                [ImGuiCol.MenuBarBg]              = {
+                    Color = {0.14, 0.14, 0.14, 1.00},
+                    PropertyName = 'MenuBarBg'
+                },
+                [ImGuiCol.ScrollbarBg]            = {
+                    Color = {0.02, 0.02, 0.02, 0.53},
+                    PropertyName = 'ScrollbarBg'
+                },
+                [ImGuiCol.ScrollbarGrab]          = {
+                    Color = {0.31, 0.31, 0.31, 1.00},
+                    PropertyName = 'ScrollbarGrab'
+                },
+                [ImGuiCol.ScrollbarGrabHovered]   = {
+                    Color = {0.41, 0.41, 0.41, 1.00},
+                    PropertyName = 'ScrollbarGrabHovered'
+                },
+                [ImGuiCol.ScrollbarGrabActive]    = {
+                    Color = {0.51, 0.51, 0.51, 1.00},
+                    PropertyName = 'ScrollbarGrabActive'
+                },
+                [ImGuiCol.CheckMark]              = {
+                    Color = {0.26, 0.59, 0.98, 1.00},
+                    PropertyName = 'CheckMark'
+                },
+                [ImGuiCol.SliderGrab]             = {
+                    Color = {0.24, 0.52, 0.88, 1.00},
+                    PropertyName = 'SliderGrab'
+                },
+                [ImGuiCol.SliderGrabActive]       = {
+                    Color = {0.26, 0.59, 0.98, 1.00},
+                    PropertyName = 'SliderGrabActive'
+                },
+                [ImGuiCol.Button]                 = {
+                    Color = {0.26, 0.59, 0.98, 0.40},
+                    PropertyName = 'Button'
+                },
+                [ImGuiCol.ButtonHovered]          = {
+                    Color = {0.26, 0.59, 0.98, 1.00},
+                    PropertyName = 'ButtonHovered'
+                },
+                [ImGuiCol.ButtonActive]           = {
+                    Color = {0.06, 0.53, 0.98, 1.00},
+                    PropertyName = 'ButtonActive'
+                },
+                [ImGuiCol.Header]                 = {
+                    Color = {0.26, 0.59, 0.98, 0.31},
+                    PropertyName = 'Header'
+                },
+                [ImGuiCol.HeaderHovered]          = {
+                    Color = {0.26, 0.59, 0.98, 0.80},
+                    PropertyName = 'HeaderHovered'
+                },
+                [ImGuiCol.HeaderActive]           = {
+                    Color = {0.26, 0.59, 0.98, 1.00},
+                    PropertyName = 'HeaderActive'
+                },
+                [ImGuiCol.Separator]              = {
+                    Color = {0.43, 0.43, 0.50, 0.50},
+                    PropertyName = 'Separator'
+                },
+                [ImGuiCol.SeparatorHovered]       = {
+                    Color = {0.10, 0.40, 0.75, 0.78},
+                    PropertyName = 'SeparatorHovered'
+                },
+                [ImGuiCol.SeparatorActive]        = {
+                    Color = {0.10, 0.40, 0.75, 1.00},
+                    PropertyName = 'SeparatorActive'
+                },
+                [ImGuiCol.ResizeGrip]             = {
+                    Color = {0.26, 0.59, 0.98, 0.20},
+                    PropertyName = 'ResizeGrip'
+                },
+                [ImGuiCol.ResizeGripHovered]      = {
+                    Color = {0.26, 0.59, 0.98, 0.67},
+                    PropertyName = 'ResizeGripHovered'
+                },
+                [ImGuiCol.ResizeGripActive]       = {
+                    Color = {0.26, 0.59, 0.98, 0.95},
+                    PropertyName = 'ResizeGripActive'
+                },
+                [ImGuiCol.Tab]                    = {
+                    Color = {0.18, 0.35, 0.58, 0.86},
+                    PropertyName = 'Tab'
+                },
+                [ImGuiCol.TabHovered]             = {
+                    Color = {0.26, 0.59, 0.98, 0.80},
+                    PropertyName = 'TabHovered'
+                },
+                [ImGuiCol.TabActive]              = {
+                    Color = {0.20, 0.41, 0.68, 1.00},
+                    PropertyName = 'TabActive'
+                },
+                [ImGuiCol.TabUnfocused]           = {
+                    Color = {0.07, 0.10, 0.15, 0.97},
+                    PropertyName = 'TabUnfocused'
+                },
+                [ImGuiCol.TabUnfocusedActive]     = {
+                    Color = {0.14, 0.26, 0.42, 1.00},
+                    PropertyName = 'TabUnfocusedActive'
+                },
+                [ImGuiCol.DockingPreview]         = {
+                    Color = {0.26, 0.59, 0.98, 0.70},
+                    PropertyName = 'DockingPreview'
+                },
+                [ImGuiCol.DockingEmptyBg]         = {
+                    Color = {0.20, 0.20, 0.20, 1.00},
+                    PropertyName = 'DockingEmptyBg'
+                },
+                [ImGuiCol.PlotLines]              = {
+                    Color = {0.61, 0.61, 0.61, 1.00},
+                    PropertyName = 'PlotLines'
+                },
+                [ImGuiCol.PlotLinesHovered]       = {
+                    Color = {1.00, 0.43, 0.35, 1.00},
+                    PropertyName = 'PlotLinesHovered'
+                },
+                [ImGuiCol.PlotHistogram]          = {
+                    Color = {0.90, 0.70, 0.00, 1.00},
+                    PropertyName = 'PlotHistogram'
+                },
+                [ImGuiCol.PlotHistogramHovered]   = {
+                    Color = {1.00, 0.60, 0.00, 1.00},
+                    PropertyName = 'PlotHistogramHovered'
+                },
+                [ImGuiCol.TableHeaderBg]          = {
+                    Color = {0.19, 0.19, 0.20, 1.00},
+                    PropertyName = 'TableHeaderBg'
+                },
+                [ImGuiCol.TableBorderStrong]      = {
+                    Color = {0.31, 0.31, 0.35, 1.00},
+                    PropertyName = 'TableBorderStrong'
+                },
+                [ImGuiCol.TableBorderLight]       = {
+                    Color = {0.23, 0.23, 0.25, 1.00},
+                    PropertyName = 'TableBorderLight'
+                },
+                [ImGuiCol.TableRowBg]             = {
+                    Color = {0.00, 0.00, 0.00, 0.00},
+                    PropertyName = 'TableRowBg'
+                },
+                [ImGuiCol.TableRowBgAlt]          = {
+                    Color = {1.00, 1.00, 1.00, 0.06},
+                    PropertyName = 'TableRowBgAlt'
+                },
+                [ImGuiCol.TextSelectedBg]         = {
+                    Color = {0.26, 0.59, 0.98, 0.35},
+                    PropertyName = 'TextSelectedBg'
+                },
+                [ImGuiCol.DragDropTarget]         = {
+                    Color = {1.00, 1.00, 0.00, 0.90},
+                    PropertyName = 'DragDropTarget'
+                },
+                [ImGuiCol.NavHighlight]           = {
+                    Color = {0.26, 0.59, 0.98, 1.00},
+                    PropertyName = 'NavHighlight'
+                },
+                [ImGuiCol.NavWindowingHighlight]  = {
+                    Color = {1.00, 1.00, 1.00, 0.70},
+                    PropertyName = 'NavWindowingHighlight'
+                },
+                [ImGuiCol.NavWindowingDimBg]      = {
+                    Color = {0.80, 0.80, 0.80, 0.20},
+                    PropertyName = 'NavWindowingDimBg'
+                },
+                [ImGuiCol.ModalWindowDimBg]       = {
+                    Color = {0.80, 0.80, 0.80, 0.35},
+                    PropertyName = 'ModalWindowDimBg'
+                }
             },
             ['Style'] = {
             },
